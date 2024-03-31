b650b0caebf54e8f0373afa7765d387a3ccecca3
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2024-04-01 00:09:36 +0200

    server: implement scoring

--- server24hc/serv/models.py
+++ server24hc/serv/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from .maputils import Map as MapUtils
+from .bruteforce_solve import bruteforce_solve
 
 class Team(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,17 +13,38 @@ class Team(models.Model):
 
     @property
     def score_player(self):
-        # implement
-        return 0
+        score = 0
+        for s in Stage.objects.all():
+            for m in s.maps.all():
+                g = Game.objects.filter(map=m, finished=True, stage=s, player=self.user).order_by('-completed_at').first()
+                if g is None:
+                    continue
+                best_score = Game.objects.filter(map=m, finished=True, victory=True, stage=s).order_by('reference_score').first().reference_score
+                if g.victory:
+                    current_player_score = g.reference_score
+                    score += current_player_score - best_score
+                else:
+                    worst_score = Game.objects.filter(map=m, finished=True, victory=True, stage=s).order_by('-reference_score').first().reference_score
+                    score += 5 + 2*(worst_score - best_score)
+        return score
 
     @property
     def score_game(self):
-        # implement
-        return 0
+        score = 0
+        for s in Stage.objects.all():
+            for m in s.maps.filter(proposed_by=self.user):
+                winning_games = Game.objects.filter(map=m, finished=True, victory=True, stage=s).count()
+                wrongly_scored_games = Score.objects.filter(game__map=m, game__stage=s, referee=self.user, valid=False).count()
+                print(wrongly_scored_games)
+                impossible_map = False#bruteforce_solve(m.map_data)[1] == []
+                number_of_games = Game.objects.filter(map=m, stage=s).count()
+
+                score += winning_games + 10*(wrongly_scored_games > 0) + impossible_map*number_of_games*10
+
+        return score
 
     @property
     def score_full(self):
-        # implement
         return self.score_player + self.score_game
 
 class Map(models.Model):
@@ -53,6 +75,7 @@ class Game(models.Model):
     requested_at = models.DateTimeField(auto_now_add=True)
     completed_at = models.DateTimeField(null=True, blank=True, default=None)
     finished = models.BooleanField(default=False)
+    victory = models.BooleanField(default=False)
     player = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player')
     reference_score = models.FloatField(null=True, blank=True)
 
@@ -66,6 +89,7 @@ class Game(models.Model):
             m = MapUtils(self.map.map_data)
             analysis_result = m.analyze_path(self.moves)
             self.reference_score = analysis_result.moves
+            self.victory = analysis_result.ok
             return analysis_result
         else:
             return None
@@ -75,8 +99,13 @@ class Game(models.Model):
 
 class Score(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    score = models.IntegerField()
+    score = models.FloatField()
     referee = models.ForeignKey(User, on_delete=models.CASCADE)
+    valid = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        self.valid = abs(self.score - self.game.reference_score)/self.game.reference_score < 0.01
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.referee.username + " - " + str(self.score)
