17785726262b093e1cfd914727fcd01ad9eb24db
JackDesBwa <jackdesbwa@gmail.com>
2024-04-07 07:04:45 +0200

    docker: update update script

--- docker/update-on-server.sh
+++ docker/update-on-server.sh
@@ -2,5 +2,5 @@
 
 sudo docker stop haum-24hc24-django
 git pull --ff-only
-sudo docker run --rm -v /var/www/odysseehaumere.haum.org/:/persist haum/24hc24-django sh -c 'python3 /persist/24hc24/server24hc/manage.py migrate'
+sudo docker run --rm --network=24hc24 -v /var/www/odysseehaumere.haum.org/:/persist haum/24hc24-django sh -c 'python3 /persist/24hc24/server24hc/manage.py migrate'
 sudo docker run --rm -d -p 127.0.0.1:83:8000 --network=24hc24 -v /var/www/odysseehaumere.haum.org/:/persist --name haum-24hc24-django haum/24hc24-django /persist/24hc24/docker/app.sh
