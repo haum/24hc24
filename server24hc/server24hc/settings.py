a2950cb4e3e72d60e81b59451b195387136b0282
JackDesBwa <jackdesbwa@gmail.com>
2024-04-07 06:39:47 +0200

    server: move database to postgres

--- server24hc/server24hc/settings.py
+++ server24hc/server24hc/settings.py
@@ -79,12 +79,24 @@ WSGI_APPLICATION = 'server24hc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if is_production:
+	DATABASES = {
+	    'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'odyssee',                                             
+		'USER': 'haum',
+		'PASSWORD': 'Poseidon',
+		'HOST': 'postgres_24hc',
+		'PORT': '5432',
+	    }
+	}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
