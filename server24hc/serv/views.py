f0cd997bac52c9589d2a6917c2d3b0bb84b789a0
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2024-04-07 08:11:11 +0200

    serv:limit the number of shown games to 500

--- server24hc/serv/views.py
+++ server24hc/serv/views.py
@@ -40,7 +40,7 @@ class ListGamesView(ListView):
     model = Game
 
     def get_queryset(self):
-        return Game.objects.all().order_by('-completed_at')
+        return Game.objects.all().order_by('-completed_at')[:500]
 
 class ListMapsView(ListView):
     model = Map
