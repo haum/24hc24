b79737842f8968b6b4d3503a1ac35507c252019e
JackDesBwa <jackdesbwa@gmail.com>
2024-04-07 06:45:46 +0200

    docker: change update for new database

--- docker/update-on-server.sh
+++ docker/update-on-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker stop haum-24hc24-django
+sudo docker stop haum-24hc24-django
 git pull --ff-only
-docker run --rm -v /var/www/odysseehaumere.haum.org/:/persist haum/24hc24-django sh -c 'python3 /persist/24hc24/server24hc/manage.py migrate'
-docker run --rm -d -p 127.0.0.1:83:8000 -v /var/www/odysseehaumere.haum.org/:/persist --name haum-24hc24-django haum/24hc24-django /persist/24hc24/docker/app.sh
+sudo docker run --rm -v /var/www/odysseehaumere.haum.org/:/persist haum/24hc24-django sh -c 'python3 /persist/24hc24/server24hc/manage.py migrate'
+sudo docker run --rm -d -p 127.0.0.1:83:8000 --network=24hc24 -v /var/www/odysseehaumere.haum.org/:/persist --name haum-24hc24-django haum/24hc24-django /persist/24hc24/docker/app.sh
